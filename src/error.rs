f1fad90eb0f94d47e7504abe9700b8d44e2393d6
Gray Olson <gray@grayolson.com>
2018-03-10 01:22:48 -0700

    logging implemented

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,11 @@
 
 error_chain!{
-    types{}
-    links{}
-    foreign_links{}
     errors{}
+    links{
+    }
+    foreign_links{
+        FernInitError(::fern::InitError) #[doc = "An error occured while setting up fern"];
+        SetLoggerError(::log::SetLoggerError) #[doc = "An error occured while setting the logger"];
+        IoError(::std::io::Error) #[doc = "An IO error occurred"];
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ extern crate tokio_io;
 extern crate nano_lib_rs;
 
 #[macro_use]
-extern crate slog;
-extern crate sloggers;
+extern crate log;
+extern crate fern;
+extern crate chrono;
 
 #[macro_use]
 extern crate error_chain;
@@ -14,6 +15,67 @@ extern crate error_chain;
 mod error;
 use error::*;
 
+fn setup_logger() -> Result<()> {
+    use std::fs::create_dir;
+    let base_path: &str = match create_dir("log") {
+        Ok(_) => {
+            "log/"
+        },
+        Err(e) => {
+            if e.kind() == std::io::ErrorKind::AlreadyExists {
+                "log/"
+            } else {
+                ""
+            }
+        }
+    };
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file(format!("{}nano-rs__{}.log", base_path, chrono::Local::now().format("%Y-%m-%d__%H-%M-%S")))?)
+        .apply()?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Setup logger
+    if let Err(e) = setup_logger() {
+        use std::io::Write;
+        let stderr = &mut ::std::io::stderr();
+        let errmsg = "Error writing to stderr";
+
+        writeln!(stderr, "Error while initializing logger: {}", e).expect(errmsg);
+    }
+
+    // Run program and log errors from error-chain using logger
+    if let Err(ref e) = run() {
+
+        error!("Failed with error: {}", e);
+
+        for e in e.iter().skip(1) {
+            error!("Caused by: {}", e);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            error!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    info!("Starting nano-rs!");
+
+    info!("Stopping nano-rs!");
+    Ok(())
 }
